93ead6f903a43498aea54934ba9db678ed979827
Rianne McCartney <riannemccartney@hotmail.com>
2019-08-18 22:39:10 +0100

    users can add comment and delete comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'home#index'
   devise_for :users
-  resources :posts, only: [:new, :create]
+  resources :posts, only: [:new, :create] do
+    resources :comments
+  end
   resources :users, only: [:show, :edit, :update]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
